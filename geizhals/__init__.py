8c109ef47c88f2bc788e72e2215fbaab9b2d733e
Julian Kahnert <mail@juliankahnert.de>
2018-08-17 22:41:14 +0200

    fix some package building stuff

--- geizhals/__init__.py
+++ geizhals/__init__.py
@@ -1,4 +1,10 @@
 """Init file for Geizhals."""
 from .geizhals import (Geizhals, Device)
+from pkg_resources import get_distribution, DistributionNotFound
 
 NAME = "geizhals"
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
